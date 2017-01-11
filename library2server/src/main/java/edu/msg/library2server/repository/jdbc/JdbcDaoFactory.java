0d471e668445ef797d3c4e8da9848d49ced556b9
Nagy Zoltan <zoltan.nagy@msg-sytems.com>
2017-01-11 15:36:21 +0200

    ok2

--- library2server/src/main/java/edu/msg/library2server/repository/jdbc/JdbcDaoFactory.java
+++ library2server/src/main/java/edu/msg/library2server/repository/jdbc/JdbcDaoFactory.java
@@ -1,12 +1,18 @@
 package edu.msg.library2server.repository.jdbc;
 
 import edu.msg.library2server.repository.DaoFactory;
+import edu.msg.library2server.repository.PublicationDao;
 import edu.msg.library2server.repository.UserDao;
 
 public class JdbcDaoFactory extends DaoFactory {
 	@Override
 	public UserDao getUserDao() {
 		return new JdbcUserDao();
+	}
+
+	@Override
+	public PublicationDao getPublicationDao() {
+		return new JdbcPublicationDao();
 	}
 	
 
---
+++ library2server/src/main/java/edu/msg/library2server/repository/jdbc/JdbcPublicationDao.java
@@ -0,0 +1,36 @@
+package edu.msg.library2server.repository.jdbc;
+
+import java.util.List;
+
+import edu.msg.library2common.model.Publication;
+import edu.msg.library2common.model.User;
+import edu.msg.library2server.repository.PublicationDao;
+
+public class JdbcPublicationDao implements PublicationDao {
+
+	public List<User> getAllPublications() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public User getPublicationByName(String publication_name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public User insertPublication(Publication pub) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void updatePublication(Publication pub) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void deletePublication(Publication pub) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
