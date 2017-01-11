ec3655c1e60ae8f9ac939937d5b6834ebbf13141
Nagy Szilard <nagy_szilardsandor@example.com>
2017-01-11 13:57:53 +0100

    pom xml

--- library2server/src/main/java/edu/msg/library2server/ServerMain.java
+++ library2server/src/main/java/edu/msg/library2server/ServerMain.java
@@ -1,9 +1,14 @@
 package edu.msg.library2server;
 
+import edu.msg.library2server.repository.jdbc.SqlHandler;
+
 public class ServerMain {
 
 	public static void main(String[] args) {
-		
+		SqlHandler cHandler;
+		while(true){
+		cHandler = SqlHandler.getInstance();
+		}
 
 	}
 
