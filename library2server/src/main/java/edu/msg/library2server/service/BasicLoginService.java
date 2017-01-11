c01917e4b90ae7d70c33df01ace1d2a9311ce4c9
gallb <barna.gall@msg-systems.com>
2017-01-11 14:30:20 +0200

    tempCommit

--- library2server/src/main/java/edu/msg/library2server/service/BasicLoginService.java
+++ library2server/src/main/java/edu/msg/library2server/service/BasicLoginService.java
@@ -6,6 +6,8 @@ package edu.msg.library2server.service;
 import java.rmi.RemoteException;
 
 import edu.msg.library2common.service.rmi.LoginServiceRmi;
+import edu.msg.library2server.repository.DaoFactory;
+import edu.msg.library2server.repository.UserDao;
 
 /**
  * @author gallb
@@ -13,8 +15,19 @@ import edu.msg.library2common.service.rmi.LoginServiceRmi;
  */
 public class BasicLoginService implements LoginServiceRmi{
 
+	private UserDao userDAO;
+	
+	public BasicLoginService() {
+		try {
+			userDAO = DaoFactory.getDaoFactory().getUserDao();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	public String login(String userName, String pwd) throws RemoteException {
-		// TODO Auto-generated method stub
+		userDAO.verifyUser(userName, pwd);
 		return null;
 	}
 }
