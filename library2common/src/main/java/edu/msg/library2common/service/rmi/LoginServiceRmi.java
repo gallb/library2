c843d41a048c80476e09ec4cb1f3ffe42ef72177
gallb <barna.gall@msg-systems.com>
2017-01-11 13:40:50 +0200

    started login service added dependecy to pom

---
+++ library2common/src/main/java/edu/msg/library2common/service/rmi/LoginServiceRmi.java
@@ -0,0 +1,18 @@
+package edu.msg.library2common.service.rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+import edu.msg.library2common.service.ServiceException;
+
+public interface LoginServiceRmi extends Remote{
+	
+	public static final String RMI_NAME = "Login";
+	public static final int RMI_PORT = 1099;
+
+	public String login (String userName, String pwd) throws RemoteException;
+	
+	/*@ToDo
+	public String logout (String token) throws RemoteException;
+	*/
+}
---
+++ library2server/src/main/java/edu/msg/library2server/service/BasicLoginService.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.msg.library2server.service;
+
+import java.rmi.RemoteException;
+
+import edu.msg.library2common.service.rmi.LoginServiceRmi;
+
+/**
+ * @author gallb
+ *
+ */
+public class BasicLoginService implements LoginServiceRmi{
+
+	public String login(String userName, String pwd) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
