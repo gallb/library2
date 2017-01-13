e01159d28b1ac304dee488d5e07814542ac464f5
Anna Katalin Kis <Katalin.Kis@msg-systems.com>
2017-01-13 15:05:42 +0200

    interface

--- library2common/src/main/java/edu/msg/library2common/service/rmi/LoginServiceRmi.java
+++ library2common/src/main/java/edu/msg/library2common/service/rmi/LoginServiceRmi.java
@@ -7,18 +7,19 @@ import edu.msg.library2common.service.ServiceException;
 
 /**
  * Verifies login credentials
- * @param userName, password
+ * 
+ * @param userName,
+ *            password
  * @author gallb
  *
  */
-public interface LoginServiceRmi extends Remote{
-	
+public interface LoginServiceRmi extends Remote {
+
 	public static final String RMI_NAME = "Login";
 	public static final int RMI_PORT = 1099;
 
-	public String login (String userName, String pwd) throws RemoteException;
-	
-	/*@ToDo
-	public String logout (String token) throws RemoteException;
-	*/
+	public String login(String userName, String pwd) throws RemoteException;
+	/*
+	 * @ToDo public String logout (String token) throws RemoteException;
+	 */
 }
--- library2server/src/main/java/edu/msg/library2server/repository/DaoInterface.java
+++ library2server/src/main/java/edu/msg/library2server/repository/DaoInterface.java
@@ -28,7 +28,7 @@ public interface DaoInterface {
 	 * @param e - the element to be inserted (it is an object which extends the BaseEntity class)
 	 * @return - an actual object that extends the BaseEntity
 	 */
-	<X extends BaseEntity> X insert(X e);
+	<X extends BaseEntity> void insert(X e);
 	
 	/**
 	 * Updates the element given by {@link e}}
@@ -40,6 +40,6 @@ public interface DaoInterface {
 	 * Deletes the element given by {@link e}}
 	 * @param e - the element to be deleted (it is an object which extends the BaseEntity class)
 	 */
-	<X extends BaseEntity> void delete(X e);
+	void delete(String id);
 	
 }
