ff761776759f5d8d3d89d7485e2b96b594139f36
gallb <barna.gall@msg-systems.com>
2017-01-13 15:45:20 +0200

    UserService Changed return types of insert, update, delete.

--- library2common/src/main/java/edu/msg/library2common/service/rmi/LoginServiceRmi.java
+++ library2common/src/main/java/edu/msg/library2common/service/rmi/LoginServiceRmi.java
@@ -17,21 +17,9 @@ import edu.msg.library2common.util.PropertyProvider;
  */
 public interface LoginServiceRmi extends Remote{
 	
-<<<<<<< HEAD
 	public static final String RMI_NAME = PropertyProvider.INSTANCE.getProperty("login.rmi_name");
-<<<<<<< HEAD
 	//public static final int RMI_PORT =Integer.parseInt(PropertyProvider.INSTANCE.getProperty("rmi_port"));
-=======
-	public static final int RMI_PORT =Integer.parseInt(PropertyProvider.INSTANCE.getProperty("rmi_port"));
->>>>>>> branch 'backend' of https://github.com/gallb/library2
->>>>>>> branch 'backend' of https://github.com/gallb/library2.git
-=======
-	public static final String RMI_NAME = "Login"; //PropertyProvider.INSTANCE.getProperty("login.rmi_name");
-	public static final int RMI_PORT =1099;//Integer.parseInt(PropertyProvider.INSTANCE.getProperty("rmi_port"));
->>>>>>> branch 'backend' of https://github.com/gallb/library2.git
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 	/**
 	 * Returns 'Reader' if the user type is reader, 'Admin' if the user type is administrator.
 	 * 'Invalid' if the user does not exist in the database
@@ -45,17 +33,4 @@ public interface LoginServiceRmi extends Remote{
 	/*@ToDo
 	public String logout (String token) throws RemoteException;
 	*/
-=======
-	public String login(String userName, String pwd) throws RemoteException;
-	/*
-	 * @ToDo public String logout (String token) throws RemoteException;
-	 */
->>>>>>> branch 'backend' of https://github.com/gallb/library2.git
-=======
-	public String login (String userName, String pwd) throws RemoteException;
-	
-	/*@ToDo
-	public String logout (String token) throws RemoteException;
-	*/
->>>>>>> branch 'backend' of https://github.com/gallb/library2.git
 }
