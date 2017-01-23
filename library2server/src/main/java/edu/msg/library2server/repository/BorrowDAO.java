b0fa37593c43789773ad32d1c649848540a9ad53
Anna Katalin Kis <Katalin.Kis@msg-systems.com>
2017-01-23 08:01:27 +0200

    DAO comments

--- library2server/src/main/java/edu/msg/library2server/repository/BorrowDAO.java
+++ library2server/src/main/java/edu/msg/library2server/repository/BorrowDAO.java
@@ -7,5 +7,12 @@ import edu.msg.library2common.model.Publication;
 import edu.msg.library2common.model.User;
 
 public interface BorrowDAO extends DaoInterface<Borrow> {
+	/**
+	 * @author kiska
+	 * 
+	 *         Returns true if addition of new borrowing was successful, else returns false
+	 * 
+	 * @param user
+	 */
 	boolean insertBorrow(User user, Publication pub, Date borrowFrom, Date borrowUntil);
 }
