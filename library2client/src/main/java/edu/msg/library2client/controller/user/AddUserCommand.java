1fa9753648c99596f38bf6c12e4d490649bf2c22
Nagy Zoltan <zoltan.nagy@msg-sytems.com>
2017-01-26 17:54:21 +0200

    commitfor User is ok

---
+++ library2client/src/main/java/edu/msg/library2client/controller/user/AddUserCommand.java
@@ -0,0 +1,15 @@
+package edu.msg.library2client.controller.user;
+
+import edu.msg.library2client.controller.AbstractCommand;
+
+public class AddUserCommand extends AbstractCommand {
+	public AddUserCommand() {
+		super(10,"client.command.user.add","client.command.user.add.button");
+	}
+		@Override
+		public void execute() {
+			// TODO Auto-generated method stub
+
+		}
+
+	}
---
+++ library2client/src/main/java/edu/msg/library2client/controller/user/EditUserCommand.java
@@ -0,0 +1,15 @@
+package edu.msg.library2client.controller.user;
+
+import edu.msg.library2client.controller.AbstractCommand;
+
+public class EditUserCommand extends AbstractCommand {
+public EditUserCommand() {
+	super(12,"client.command.user.edit","client.command.user.edit.button");
+}
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
