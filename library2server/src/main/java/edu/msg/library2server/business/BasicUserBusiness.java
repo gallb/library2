e35104190c99fab34c1d39813c8cd3f0ea5945ba
Nagy Zoltan <zoltan.nagy@msg-sytems.com>
2017-01-26 16:30:57 +0200

    HibernateUserDao, UserBussines layer are ok

--- library2server/src/main/java/edu/msg/library2server/business/BasicUserBusiness.java
+++ library2server/src/main/java/edu/msg/library2server/business/BasicUserBusiness.java
@@ -1,45 +1,43 @@
 package edu.msg.library2server.business;
 
-import java.rmi.RemoteException;
-import java.rmi.server.RemoteObject;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import edu.msg.library2common.model.User;
-import edu.msg.library2common.service.rmi.UserServiceRmi;
 import edu.msg.library2common.util.PropertyProvider;
 import edu.msg.library2server.repository.DaoFactory;
 import edu.msg.library2server.repository.UserDao;
 import edu.msg.library2server.repository.jdbc.SqlHandlerException;
+
 /**
  * 
  * @author nagyz
  *
  */
-public class BasicUserBusiness implements UserServiceRmi{
+public class BasicUserBusiness implements UserHandlerInterface {
 	private UserDao userDao;
 	private static final Logger LOGGER = LoggerFactory.getLogger(BasicUserBusiness.class);
 
-	public BasicUserBusiness() throws RemoteException,BusinessLayerException {
+	public BasicUserBusiness() throws BusinessLayerException {
 		super();
 	}
-/**
- * Adds new user to database
- * @param entity-User
- * @return true
- * -if insert operation was successful
- * @return false
- * -if insert operation didn't executed
- */
+
+	/**
+	 * Adds new user to database
+	 * 
+	 * @param entity-User
+	 * @return true -if insert operation was successful
+	 * @return false -if insert operation didn't executed
+	 */
 	@Override
 	public boolean addNewEntity(User entity) throws BusinessLayerException {
 		boolean flag;
-		try{
-		userDao = DaoFactory.getDaoFactory().getUserDao();
-		flag= userDao.insert(entity);
-		}catch(SqlHandlerException e){
+		try {
+			userDao = DaoFactory.getDaoFactory().getUserDao();
+			flag = userDao.insert(entity);
+		} catch (SqlHandlerException e) {
 			LOGGER.error(PropertyProvider.INSTANCE.getProperty("error_logger_BasicUserBusiness"), e);
 			throw new BusinessLayerException(PropertyProvider.INSTANCE.getProperty("error_BasicUserBusiness"));
 		}
@@ -48,19 +46,18 @@ public class BasicUserBusiness implements UserServiceRmi{
 
 	/**
 	 * Updates a user in database
+	 * 
 	 * @param entity-User
-	 * @return true
-	 * -if update operation was successful
-	 * @return false
-	 * -if update operation didn't executed
+	 * @return true -if update operation was successful
+	 * @return false -if update operation didn't executed
 	 */
 	@Override
 	public boolean updateEntity(User entity) throws BusinessLayerException {
 		boolean flag;
-		try{
-		userDao = DaoFactory.getDaoFactory().getUserDao();
-		flag= userDao.update(entity);
-		}catch(SqlHandlerException e){
+		try {
+			userDao = DaoFactory.getDaoFactory().getUserDao();
+			flag = userDao.update(entity);
+		} catch (SqlHandlerException e) {
 			LOGGER.error(PropertyProvider.INSTANCE.getProperty("error_logger_BasicUserBusiness"), e);
 			throw new BusinessLayerException(PropertyProvider.INSTANCE.getProperty("error_BasicUserBusiness"));
 		}
@@ -68,60 +65,37 @@ public class BasicUserBusiness implements UserServiceRmi{
 	}
 
 	/**
-	 * Deletes a user in database by userId
+	 * Deletes a user from database by userId
+	 * 
 	 * @param entityID-String
-	 * @return true
-	 * -if delete operation was successful
-	 * @return false
-	 * -if delete operation didn't executed
+	 * @return true -if delete operation was successful
+	 * @return false -if delete operation didn't executed
 	 */
 	@Override
 	public boolean deleteEntity(String entityID) throws BusinessLayerException {
 		boolean flag;
-		try{
-		userDao = DaoFactory.getDaoFactory().getUserDao();
-		flag= userDao.delete(entityID);
-		}catch(SqlHandlerException e){
+		try {
+			userDao = DaoFactory.getDaoFactory().getUserDao();
+			flag = userDao.delete(entityID);
+		} catch (SqlHandlerException e) {
 			LOGGER.error(PropertyProvider.INSTANCE.getProperty("error_logger_BasicUserBusiness"), e);
 			throw new BusinessLayerException(PropertyProvider.INSTANCE.getProperty("error_BasicUserBusiness"));
 		}
 		return flag;
 	}
+
 	/**
-	 * Get's a user in database by userName
-	 * @param userName-String
-	 * @return User
-	 * -if user search operation was successful
-	 * @return false
-	 * -if user search operation didn't executed
+	 * Get's a List<User> from database by name
+	 * 
+	 * @param name-String
+	 * @return List<User> -if user search operation by name was successful
 	 */
-	
-	public User getByUsername(String userName) throws BusinessLayerException {
-		User user;
-		try{
-		userDao = DaoFactory.getDaoFactory().getUserDao();
-		user=userDao.getUserByUserName(userName);
-		}catch(SqlHandlerException e){
-			LOGGER.error(PropertyProvider.INSTANCE.getProperty("error_logger_BasicUserBusiness"), e);
-			throw new BusinessLayerException(PropertyProvider.INSTANCE.getProperty("error_BasicUserBusiness"));
-		}
-		return user;
-		
-	}
-	/**
-	 * Get's a user in database by userID
-	 * @param userID-String
-	 * @return User
-	 * -if user search operation by id was successful
-	 * @return false
-	 * -if user search operation by id didn't executed
-	 */
-	public User getByUserID(String userID) throws BusinessLayerException {	
-		User user;
-		try{
-		userDao = DaoFactory.getDaoFactory().getUserDao();
-		user=userDao.getById(userID);
-		}catch(SqlHandlerException e){
+	public List<User> searchForUsers(String name) throws BusinessLayerException {
+		List<User> user;
+		try {
+			userDao = DaoFactory.getDaoFactory().getUserDao();
+			user = userDao.getByName(name);
+		} catch (SqlHandlerException e) {
 			LOGGER.error(PropertyProvider.INSTANCE.getProperty("error_logger_BasicUserBusiness"), e);
 			throw new BusinessLayerException(PropertyProvider.INSTANCE.getProperty("error_BasicUserBusiness"));
 		}
@@ -129,17 +103,57 @@ public class BasicUserBusiness implements UserServiceRmi{
 	}
 
 	/**
-	 * Get's a List<User> in database by name
-	 * @param name-String
-	 * @return List<User>
-	 * -if user search operation by name was successful
+	 * Get's a List<User> from database
+	 * 
+	 * @return List<User> -if user search operation was successful
 	 */
-	public List<User> searchForUser(String name) throws BusinessLayerException {
+	@Override
+	public List<User> getAll() throws BusinessLayerException {
 		List<User> user;
-		try{
-		userDao = DaoFactory.getDaoFactory().getUserDao();
-		user=userDao.getByName(name);
-		}catch(SqlHandlerException e){
+		try {
+			userDao = DaoFactory.getDaoFactory().getUserDao();
+			user = userDao.getAll();
+		} catch (SqlHandlerException e) {
+			LOGGER.error(PropertyProvider.INSTANCE.getProperty("error_logger_BasicUserBusiness"), e);
+			throw new BusinessLayerException(PropertyProvider.INSTANCE.getProperty("error_BasicUserBusiness"));
+		}
+		return user;
+	}
+
+	/**
+	 * Get's a user in database by userID
+	 * 
+	 * @param userID-String
+	 * @return User -if user search operation by id was successful
+	 * @return false -if user search operation by id didn't executed
+	 */
+	@Override
+	public User getById(String id) throws BusinessLayerException {
+		User user;
+		try {
+			userDao = DaoFactory.getDaoFactory().getUserDao();
+			user = userDao.getById(id);
+		} catch (SqlHandlerException e) {
+			LOGGER.error(PropertyProvider.INSTANCE.getProperty("error_logger_BasicUserBusiness"), e);
+			throw new BusinessLayerException(PropertyProvider.INSTANCE.getProperty("error_BasicUserBusiness"));
+		}
+		return user;
+	}
+
+	/**
+	 * Get's a user in database by userName
+	 * 
+	 * @param userName-String
+	 * @return User -if user search operation was successful
+	 * @return false -if user search operation didn't executed
+	 */
+	@Override
+	public User getUserByUserName(String userName) throws BusinessLayerException {
+		User user;
+		try {
+			userDao = DaoFactory.getDaoFactory().getUserDao();
+			user = userDao.getUserByUserName(userName);
+		} catch (SqlHandlerException e) {
 			LOGGER.error(PropertyProvider.INSTANCE.getProperty("error_logger_BasicUserBusiness"), e);
 			throw new BusinessLayerException(PropertyProvider.INSTANCE.getProperty("error_BasicUserBusiness"));
 		}
