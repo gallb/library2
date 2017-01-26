eb2858ad548b6b681ccd1ca7b64706561fcf8eca
Nagy Szilard <nagy_szilardsandor@example.com>
2017-01-26 16:43:12 +0100

    author lekeres nev szerint also server szinten

--- library2server/src/main/java/edu/msg/library2server/service/BasicAuthorService.java
+++ library2server/src/main/java/edu/msg/library2server/service/BasicAuthorService.java
@@ -2,11 +2,19 @@ package edu.msg.library2server.service;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import edu.msg.library2common.model.Author;
+import edu.msg.library2common.service.ServiceException;
 import edu.msg.library2common.service.ServiceLayerException;
 import edu.msg.library2common.service.rmi.AuthorServiceRmi;
+import edu.msg.library2common.util.PropertyProvider;
+import edu.msg.library2server.business.AuthorHandlerInterface;
+import edu.msg.library2server.business.BusinessLayerException;
 /**
  * 
  * @author nagyz
@@ -14,6 +22,9 @@ import edu.msg.library2common.service.rmi.AuthorServiceRmi;
  */
 public class BasicAuthorService extends UnicastRemoteObject implements AuthorServiceRmi {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(BasicAuthorService.class);
+	private AuthorHandlerInterface authorHandlerInterface;
+	
 	protected BasicAuthorService() throws  ServiceLayerException,RemoteException {
 		super();
 	}
@@ -37,9 +48,15 @@ public class BasicAuthorService extends UnicastRemoteObject implements AuthorSer
 	}
 
 	@Override
-	public List<Author> serchByName(String name) throws  ServiceLayerException,RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Author> serchByName(String name) throws  ServiceException,RemoteException {
+		List<Author> authors = new ArrayList<>();
+		try{
+			authors = authorHandlerInterface.getByName(name);
+		}catch (BusinessLayerException e) {
+			LOGGER.error(PropertyProvider.INSTANCE.getProperty("error_logger_BasicAuthorService"));
+			throw new ServiceException(PropertyProvider.INSTANCE.getProperty("error_BasicAuthorService"));
+		}
+		return authors;
 	}
 
 }
